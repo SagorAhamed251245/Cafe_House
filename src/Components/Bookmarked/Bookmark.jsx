f13dd5c05043167237f3d77952835d0fe5bb0c8e
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-03-31 22:15:46 +0600

    Spent time on read done

--- src/Components/Bookmarked/Bookmark.jsx
+++ src/Components/Bookmarked/Bookmark.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Bookmark = ({blogItem}) => {
-    console.log(blogItem)
+    const blogItems = blogItem
+
+    let totalTime=0;
+
+    for( const blog of blogItems){
+
+       totalTime = blog.Read_time + totalTime;
+      
+       
+    
+    }
     return (
         <div>
 
             <div className='h-[50px] bg-[#bbbbbb]  text-blue-600 flex  justify-center items-center rounded-md'>
-                <p className='font-semibold text-base'>Spent time on read : 177 min</p>
+                <p className='font-semibold text-base'>Spent time on read: {totalTime} min</p>
             </div>
             <div>
-                <p className='font-bold text-base my-5'>Bookmarked Blogs: 1</p>
+                <p className='font-bold text-base my-5'>Bookmarked Blogs: {blogItem.length}</p>
             </div>
             <div className='h-auto '>
                 
                 {
-                    blogItem.map(item=>(<div className='h-auto p-3 bg-white  flex  justify-center items-center my-3 rounded-md'>
+                    blogItem.map((item,index)=>(<div key={index} className='h-auto p-3 bg-white  flex  justify-center items-center my-3 rounded-md'>
                         <p className='font-semibold text-base my-5'>{item.blog_title}</p>
                     </div> ))
                 }
